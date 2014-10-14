c76a4b5fb260e1b2c6b5b0303ea93e877b426c11
Martin Gammelsæter <martigam@samfundet.no>
2014-10-14 18:11:58 +0200

    Avoid creating MultiNEAT nets for every evaluation.
    Should probably not use genome_archive like this at all, but it works
    for now.

--- oeelm.py
+++ oeelm.py
@@ -23,31 +23,11 @@ class Network(object):
         self.squared_norm_ema = 0.0
         # TODO: Now that we use .Tick(), we might be able to keep track of this
         # in a better way.
-        self.genome_archive = {} # gID: [weight, weight_magnitude_ema]
+        self.genome_archive = {} # gID: [weight, weight_magnitude_ema, feature_net]
 
-    def calculate_feature_values(self, genome_list, input_vals):
+    def _calculate_feature_values(self, genome_list, input_vals):
         def evaluate(genome):
-            # TODO: Visualization of all kinds should be callable.
-            """
-            net = NEAT.NeuralNetwork()
-            genome.BuildPhenotype(net) 
-            img = np.zeros((250, 250, 3), dtype=np.uint8)
-            img += 10
-            NEAT.DrawPhenotype(img, (0, 0, 250, 250), net )
-            cv2.imshow("CPPN", img)
-            """
-
-            # TODO: See if this can be moved outside loop.
-            feature = NEAT.NeuralNetwork()
-            genome.BuildHyperNEATPhenotype(feature, self.substrate) 
-            """
-            img = np.zeros((250,250,3), dtype=np.uint8)
-            img += 10
-            NEAT.DrawPhenotype(img, (0, 0, 250, 250), feature, substrate=True)
-            cv2.imshow("NN", img)
-            cv2.waitKey(1)
-            """
-
+            feature = self.genome_archive[genome.GetID()][2]
             feature.Flush()
             feature.Input(input_vals)
             # FIXME: See if this is really necessary.
@@ -57,28 +37,23 @@ class Network(object):
 
         return np.asarray([evaluate(genome) for genome in genome_list])
 
+    def median_weight_magnitudes_ema(self):
+        return np.median([val[1] for val in self.genome_archive.values()])
+
     def train(self, genome_list, input_vals, target, iteration):
         # TODO: genome_archive is a quite ugly hack.
         assert(len(genome_list) == len(self.output_weights))
-        feature_values = self.calculate_feature_values(genome_list, input_vals)
+        feature_values = self._calculate_feature_values(genome_list, input_vals)
 
         squared_feature_norm = np.dot(feature_values, feature_values)
-        median_w_m_ema = 0
 
         if iteration == 0:
             self.squared_norm_ema = squared_feature_norm
         else:
             self.squared_norm_ema = (self.squared_norm_ema * 0.999 +
                     squared_feature_norm * 0.001)
-            median_w_m_ema = np.median([val[1] for val in self.genome_archive.values()])
 
         learning_rate = BASE_LEARNING_RATE / self.squared_norm_ema
-
-        # FIXME: With .Tick() we know that this is only one element.
-        for i in range(len(genome_list)):
-            g_id = genome_list[i].GetID()
-            if g_id not in self.genome_archive:
-                self.genome_archive[g_id] = [0.0, median_w_m_ema]
 
         output_weights = [self.genome_archive[g.GetID()][0] for g in genome_list]               
         output = np.dot(feature_values, output_weights)
@@ -109,12 +84,15 @@ class NEATOeelm(object):
     def __init__(self, neat_params, neat_genome, neat_substrate, noutputs):
         self.population = NEAT.Population(neat_genome, neat_params, True, 1.0)
         self.noutputs = noutputs
+        self.substrate = neat_substrate
         self.net = Network(neat_substrate, neat_params.PopulationSize)
         # TODO: Should this be here or left to caller?
         self.generation = 0
 
     def train(self, input_data, target):
         genome_list = NEAT.GetGenomeList(self.population)
+        if self.generation == 0:
+            self._init_genome_archive(genome_list)
         assert(len(genome_list) == self.net.nfeatures)
 
         squared_error, output = self.net.train(genome_list, input_data, target,
@@ -124,10 +102,30 @@ class NEATOeelm(object):
         deleted_genome = NEAT.Genome()
         new_genome = self.population.Tick(deleted_genome)
         del self.net.genome_archive[deleted_genome.GetID()]
+        # TODO: Move to function. Does same in _init_genome_archive
+        feature = NEAT.NeuralNetwork()
+        new_genome.BuildHyperNEATPhenotype(feature, self.substrate)
+        self.net.genome_archive[new_genome.GetID()] = [
+                0.0,
+                self.net.median_weight_magnitudes_ema(),
+                feature     
+        ]
 
         self.generation += 1
 
         return (squared_error, output)
+
+    def _init_genome_archive(self, genome_list):
+        for genome in genome_list:
+            g_id = genome.GetID()
+            if g_id not in self.net.genome_archive:
+                feature = NEAT.NeuralNetwork()
+                genome.BuildHyperNEATPhenotype(feature, self.substrate) 
+                self.net.genome_archive[g_id] = [
+                        0.0,
+                        0.0,
+                        feature
+                ]
 
     def _zip_fitness(self, genome_list):
         for genome in genome_list:
