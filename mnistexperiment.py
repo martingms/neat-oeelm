cb912ba7218a28a6ac7a643bca0b0a972d9a65fb
Martin Gammelsæter <martigam@samfundet.no>
2014-10-17 16:52:44 +0200

    Do test on MNIST test set.
    This exact code/configuration yields around ~13% error on the test set.

--- mnistexperiment.py
+++ mnistexperiment.py
@@ -9,7 +9,7 @@ from mnist import mnist_substrate, load_mnist
 random.seed(int(time.time()))
 
 params = NEAT.Parameters()
-params.PopulationSize = 100
+params.PopulationSize = 1000
 params.YoungAgeFitnessBoost = 1.0
 
 params.MutateActivationAProb = 0.0
@@ -55,14 +55,45 @@ images /= 255.0
 labeltranslations = np.identity(10)
 
 splitlabels = np.zeros((len(labels), 10))
-for i in range(len(labels)):
+for i in xrange(len(labels)):
     splitlabels[i] = labeltranslations[labels[i]]
-    
-for generation in range(len(images)):
+
+for generation in xrange(len(images)):
     err, output = no.train(images[generation], splitlabels[generation])
 
+    print "E1:Generation:", generation, "/", len(images)
+
+for generation in xrange(len(images)):
+    err, output = no.train(images[generation], splitlabels[generation])
+
+    print "E2:Generation:", generation, "/", len(images)
+    """
     print "========================================="
     print "INPUT:", "... some image"
     print "TARGET:", splitlabels[generation], labels[generation]
     print "OUTPUT:", output
     print "SQUARED_ERROR:", err
+    """
+
+images, labels = load_mnist("testing", path="mnistdata")
+images /= 255.0
+
+correct, wrong = 0, 0
+for test in xrange(len(images)):
+    output = no.activate(images[test])
+
+    o = 0
+    imax = float("-inf")
+    for i in xrange(len(output)):
+        if output[i] > imax:
+            imax = output[i]
+            o = i
+
+    if o == labels[test]:
+        correct += 1
+    else:
+        wrong += 1
+
+    print "Test:", test, "/", len(images)
+
+print "Correct:", correct, "Wrong:", wrong, "Total:", len(images)
